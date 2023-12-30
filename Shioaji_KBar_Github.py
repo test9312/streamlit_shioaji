ab327d2809568585eacf582e48aa1fcf2c80f984
Lee <101623099+test9312@users.noreply.github.com>
2023-12-31 03:19:56 +0800

    Update Shioaji_KBar_Github.py

--- Shioaji_KBar_Github.py
+++ Shioaji_KBar_Github.py
@@ -126,8 +126,6 @@ import numpy as np
 def calculate_rsi(close_prices, period=14):
     # 計算每日價格變動
     delta = np.diff(df['close'])
-    # 將第一個元素補為0，使長度保持一致
-    delta = np.concatenate(([0], np.diff(df['close'])))
 
     # 計算正價格變動的平均值（gain）
     gain = np.where(delta > 0, delta, 0)
@@ -140,6 +138,9 @@ def calculate_rsi(close_prices, period=14):
     # 避免除以0的情況
     loss[loss == 0] = np.nan
 
+    # 將 close_prices 調整為相同的長度
+    df['close'] = df['close'][-len(gain):]
+	
     # 計算相對強弱指數（RS）
     rs = gain / loss
 
