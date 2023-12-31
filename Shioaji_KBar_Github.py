766efb6063cb4befd163dc21f33f58a286f4bb18
Lee <101623099+test9312@users.noreply.github.com>
2023-12-31 22:16:28 +0800

    Update Shioaji_KBar_Github.py

--- Shioaji_KBar_Github.py
+++ Shioaji_KBar_Github.py
@@ -247,7 +247,7 @@ with st.expander("布林通道"):
     
     fig3.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_bollinger_bands+1:], y=KBar_df['SMA'][last_nan_index_bollinger_bands+1:], mode='lines',line=dict(color='black', width=2), name='SMA'), 
                   secondary_y=True)
-    fig3.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_bollinger_bands+1:], y=KBar_df.loc[:,'upperband'][last_nan_index_bollinger_bands+1:], mode='lines',line=dict(color='red', width=2), name='upperband'), 
+    fig3.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_bollinger_bands+1:], y=KBar_df[:,13][last_nan_index_bollinger_bands+1:], mode='lines',line=dict(color='red', width=2), name='upperband'), 
                   secondary_y=True)
     fig3.add_trace(go.Scatter(x=KBar_df['Time'][last_nan_index_bollinger_bands+1:], y=KBar_df['lowerband'][last_nan_index_bollinger_bands+1:], mode='lines',line=dict(color='blue', width=2), name='lowerband'), 
                   secondary_y=True)
